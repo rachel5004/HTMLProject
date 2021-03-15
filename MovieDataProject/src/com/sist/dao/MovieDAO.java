f4371f1497e4922e73fae820b2ef054f8e7548d0
rachel5004 <75432228+rachel5004@users.noreply.github.com>
2021-03-15 17:53:04 +0900

    js

--- MovieDataProject/src/com/sist/dao/MovieDAO.java
+++ MovieDataProject/src/com/sist/dao/MovieDAO.java
@@ -92,5 +92,61 @@ public class MovieDAO {
 		}
 		return list;
 	}   
+	// 영화 상세보기
+  /*
+   *   JDBC (DAO) => 오라클 열기 : SQL전송 : 실행 (결과값) => 오라클 닫기
+   *                 ========                          ======== 시간이 오래 걸린다
+   *    => DBCP      => 미리 Connection생성 => 주소연결
+   *    => ORM (실무) => 마이바티스
+   *    
+   *    MNO       NOT NULL NUMBER         
+		CNO                NUMBER         
+		POSTER    NOT NULL VARCHAR2(260)  
+		TITLE     NOT NULL VARCHAR2(200)  
+		REGDATE   NOT NULL VARCHAR2(200)  
+		GENRE     NOT NULL VARCHAR2(100)  
+		NATION    NOT NULL VARCHAR2(50)   
+		GRADE     NOT NULL VARCHAR2(50)   
+		TIME      NOT NULL VARCHAR2(50)   
+		SCORE              NUMBER(2,1)    
+		SHOWUSER           VARCHAR2(30)   
+		BOXOFFICE          VARCHAR2(10)   
+		STORY              CLOB           
+		KEY                VARCHAR2(30)
+   */
+  public MovieVO movieDetailData(int mno) {
+	  MovieVO vo=new MovieVO();
+	  try {
+		  getConnection();
+		  String sql="SELECT mno,poster,title,regdate,genre,nation,grade,"
+				    +"time,score,showuser,boxoffice,story,key "
+				    +"FROM daum_movie "
+				    +"WHERE mno=?";
+		  ps=conn.prepareStatement(sql);
+		  ps.setInt(1, mno);
+		  ResultSet rs=ps.executeQuery();
+		  rs.next();
+		  vo.setMno(rs.getInt(1));
+		  vo.setPoster(rs.getString(2));
+		  vo.setTitle(rs.getString(3));
+		  vo.setRegdate(rs.getString(4));
+		  vo.setGenre(rs.getString(5));
+		  vo.setNation(rs.getString(6));
+		  vo.setGrade(rs.getString(7));
+		  vo.setTime(rs.getString(8));
+		  vo.setScore(rs.getDouble(9));
+		  vo.setShowUser(rs.getString(10));
+		  vo.setBoxoffice(rs.getString(11));
+		  vo.setStory(rs.getString(12));
+		  vo.setKey(rs.getString(13));
+		  
+		  rs.close();
+	  }catch(Exception ex) {
+		  ex.printStackTrace();
+	  }finally {
+		  disConnection();
+	  }
+	  return vo;
+  }
 
 }
